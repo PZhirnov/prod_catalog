23e012dc402fdabf22caf131c213684b8ad4a3d2
Pavel <pavel-zh@inbox.ru>
2022-06-02 15:58:19 +0300

    add SECRET in os.environ

--- catalog/mainapp/models/catalog.py
+++ catalog/mainapp/models/catalog.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.sites.models import Site
 from django.contrib.sites.managers import CurrentSiteManager
+from django.db.models import Manager
 
 # Create your models here.
 
@@ -8,7 +9,10 @@ from django.contrib.sites.managers import CurrentSiteManager
 class Catalog(models.Model):
     title = models.CharField(max_length=64, unique=True, null=False)
     site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
+    # objects = Manager()  # если нужен object в ORM, а не сайт - т.е. выводит со всех сайтов
     on_site = CurrentSiteManager('site')
+    # или так
+    # objects = CurrentSiteManager('site')
 
     def __str__(self):
         return self.title
